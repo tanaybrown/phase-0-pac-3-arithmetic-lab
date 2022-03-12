27072ec1970ab759706afb18c99e9652e85ed465
Tanya <tanaybrown@gmail.com>
2022-03-11 20:31:31 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(a, b){
+    return (a + b)
+}
+function subtract(a, b) {
+    return (a - b)
+}
+function multiply (a, b) {
+    return (a * b)
+}
+function divide (a, b) {
+    return (a / b)
+}
+function increment(n) {
+    n++
+    return n
+}
+function decrement(n){
+    n--
+    return n
+}
+function makeInt(n) {
+    return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+    console.log(parseFloat("2.222"))
+    console.log(parseFloat('sldkjflksjf'))
+    return parseFloat(n)
+}
